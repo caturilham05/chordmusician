8f0732b74604442435b32f1ff88819453c3a64d2
catur <catur@fisip.net>
2025-10-27 01:12:13 +0700

    feat: add favicon

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -7,6 +7,7 @@
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/favicon.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <title>{{str_replace('-', ' ', config('app.name'))}} | {{$title ?? ''}}</title>
