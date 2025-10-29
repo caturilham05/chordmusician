a2ab6366568d52a206aeb026a538aa4159a60f38
catur <catur@fisip.net>
2025-10-29 12:32:06 +0700

    fix

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('click:reset')->dailyAt('12:25');
+// Schedule::command('click:reset')->dailyAt('12:25');
+Schedule::call(function () {
+    Artisan::call('click:reset');
+})->dailyAt('12:35');
