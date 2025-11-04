c7c766b20892a5addb515ac6d97f0ffb892bfb63
catur <catur@fisip.net>
2025-11-04 22:52:28 +0700

    refactor: favicon admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->favicon(asset('favicon.png'))
+            ->favicon(asset('favicon.ico'))
             ->brandName('Chord Musician')
             ->login()
             ->colors([
